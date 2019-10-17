65bc3c37b3433ad6deb35115b90f92138983144f
jaime <jaime@150.128.161.38>
2019-10-17 16:33:19 +0200

    FormaComponent creado

---
+++ DibujaFormas/src/Formas/FormaComponent.java
@@ -0,0 +1,22 @@
+package Formas;
+
+public abstract class FormaComponent {
+
+	public void dibuja() {
+		
+	}
+	
+	public void desplazaRelativo(int x, int y) {
+		
+	}
+	
+	public void anade(FormaComponent fc) {
+		
+	}
+	
+	public boolean elimina(FormaComponent fc) {
+		
+		return false;
+	}
+	
+}
